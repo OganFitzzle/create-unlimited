053150080ddcce40179aa879b4672731e321cb22
rhysdh540 <rhys_dehaan@ryecountryday.org>
2023-02-07 16:17:33 -0500

    initial commit

--- fabric/src/main/java/net/rdh/createunlimited/fabric/ExpectPlatformThingyImpl.java
+++
@@ -1,9 +0,0 @@
-package net.rdh.createunlimited.fabric;
-
-import net.fabricmc.loader.api.FabricLoader;
-
-public class ExpectPlatformThingyImpl {
-	public static String platformName() {
-		return FabricLoader.getInstance().isModLoaded("quilt_loader") ? "Quilt" : "Fabric";
-	}
-}
--- forge/src/main/java/net/rdh/createunlimited/forge/ExpectPlatformThingyImpl.java
+++
@@ -1,7 +0,0 @@
-package net.rdh.createunlimited.forge;
-
-public class ExpectPlatformThingyImpl {
-	public static String platformName() {
-		return "Forge";
-	}
-}
